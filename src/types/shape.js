b5a20a4d9025389524b3ffc3678e4b081e6bcbc7
Lukáš Mačuda <lukas.macuda@gmail.com>
2018-05-17 15:25:48 +0200

    added forgotten shape file

---
+++ src/types/shape.js
@@ -0,0 +1,39 @@
+import Error from 'smart-error';
+import Base from './base/nullable';
+
+import Type from './base';
+
+export default class Shape extends Base {
+
+    _shape = null;
+
+    constructor(shape) {
+        super();
+        Object.keys(shape).forEach((key) => {
+            const type = shape[key];
+            if (!(type instanceof Type)) {
+                throw new Error(`The type ${type} must be an instance of Type.`, 'unsupported_operation');
+            }
+        })
+        this._shape = shape;
+    }
+
+    cast(value) {
+        const o = {};
+        Object.keys(this._shape).forEach((key) => {
+            const type = this._shape[key];
+            if (!value[key]) {
+                throw new Error(`Missing key '${key}' in the shape.`, 'unsupported_operation');
+            }
+            try {
+                o[key] = type.cast(value[key]);
+            } catch (e) {
+                if (e.code === 'ERR_INVALID_CAST') {
+                    throw new Error(`The value at key '${key}' cannot be cast to ${type.getName().toLowerCase()}.`, 'invalid_cast');
+                }
+                throw e;
+            }
+        });
+        return o;
+    }
+}
